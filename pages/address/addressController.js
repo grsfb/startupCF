2f113811b9846db8ac5c5715916fad941f6ced06
‘gaurav’ <gauravorgrs@gmail.com>
2015-11-29 19:26:43 +0530

    Adding changes for address

--- pages/address/addressController.js
+++ pages/address/addressController.js
@@ -3,45 +3,81 @@
     angular
         .module('Chefonia')
         .controller('AddressController', AddressController);
-    AddressController.$inject = ['$rootScope', '$location', 'AddressService'];
+    AddressController.$inject = ['$rootScope', '$window', 'AddressService'];
 
-    function AddressController($rootScope, $location, AddressService) {
+    function AddressController($rootScope, $window, AddressService) {
         var vm = this;
-        vm.showAddressEditor=true;
-        vm.allAddress=[];
-        vm.address={};
-        vm.enableAddressEditor=enableAddressEditor;
-        vm.saveAndClose=saveAndClose;
-        vm.save=save;
-        vm.cancel=cancel;
-        vm.setShippingAddress=setShippingAddress;
-        $rootScope.selectedAddress=undefined;
+        vm.showAddressEditor = true;
+        vm.allAddress = [];
+        vm.address = {};
+        vm.enableAddressEditor = enableAddressEditor;
+        vm.saveAndClose = saveAndClose;
+        vm.save = save;
+        vm.cancel = cancel;
+        vm.setSelectedAddress = setSelectedAddress;
+        vm.setShippingAddress = setShippingAddress;
+        vm.deleteAddress = deleteAddress;
+        $rootScope.selectedAddress = undefined;
 
-        function setShippingAddress(address){
-            $rootScope.selectedAddress=address;
-        }
+        AddressService.getAllAddress("9158100044", function (response) {
+            if(response.success){
+                vm.allAddress = response.data;
+            }else{
+                //show error msg
+            }
 
-        AddressService.getAllAddress("userId",function(response){
-            vm.allAddress=response;
         });
 
-        function enableAddressEditor(){
-            vm.showAddressEditor=false;
+        function setShippingAddress(address) {
+            $rootScope.selectedAddress = address;
         }
 
-        function saveAndClose(){
-            AddressService.create("userId",address)
-            vm.showAddressEditor=true;
+
+        function setSelectedAddress(address, index, selectedIndex) {
+            vm.selectedIndex = index;
         }
 
-        function cancel(){
-            vm.address={};
-            vm.showAddressEditor=true;
+        function enableAddressEditor() {
+            vm.showAddressEditor = false;
         }
-        function save(){
+
+        function deleteAddress(index) {
+            if ($window.confirm("Do you want to continue?")) {
+                AddressService.remove(vm.allAddress[index]);
+                vm.allAddress.splice(index, 1);
+            }
+        }
+
+        function saveAndClose(address) {
+            AddressService.create(address, function(response){
+                if(response.success){
+                    //msg to show
+                }
+            });
+
+            vm.showAddressEditor = true;
+        }
+
+        function cancel() {
+            vm.address = {};
+            vm.showAddressEditor = true;
+        }
+
+        function save() {
             vm.allAddress.push(vm.address);
-            vm.showAddressEditor=false;
-            vm.address={};
+            vm.showAddressEditor = false;
+            vm.address = {};
+        }
+
+        function Address(userId, fullName, lineOne, lineTwo, city, state, zip, mobile) {
+            this.userId = userId;
+            this.fullName = fullName;
+            this.lineOne = lineOne;
+            this.lineTwo = lineTwo;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.mobile = mobile;
         }
     }
 })();
--- services/addressService.js
+++ services/addressService.js
@@ -5,28 +5,30 @@
         .factory('AddressService', AddressService);
     AddressService.$inject = ['$http', '$rootScope'];
     function AddressService($http, $rootScope) {
+        var baseURL="http://localhost:8080";
         var service = {};
         service.create = create;
         service.remove=remove;
         service.getAllAddress=getAllAddress;
 
         return service;
-        function create(itemId,userId,callback) {
+        function create(address,callback) {
             var response;
-            $http.post('/api/users/userId/address', {"itemId":itemId,"userId":userId})
-                .success(function (response) {
+            $http.post(baseURL+'/address/add', {"address":address})
+                .success(function (res) {
+                    response={success:true,data:res};
                     callback(response);
                 })
-                .error(function () {
-                    response = {success: false, message: 'Username or password is incorrect'};
+                .error(function (res) {
+                    response = {success: false, data:res};
                     callback(response);
                 });
         }
 
-        function remove(itemId,userId,callback){
+        function remove(addressId,callback){
             var response;
             //TODO: delete method call
-            $http.post('/api/users/delete', {"itemId":itemId,"userId":userId})
+            $http.delete(baseURL+'/address/delete/'+addressId)
                 .success(function (response) {
                     callback(response);
                 })
@@ -38,12 +40,13 @@
 
         function getAllAddress(userId,callback){
             var response;
-            $http.get('/client/data/address.json')
-                .success(function (response) {
+            $http.get(baseURL+'/address/'+userId+'/all')
+                .success(function (res) {
+                    response={success:true,data:res};
                     callback(response);
                 })
-                .error(function () {
-                    response = {success: false, message: 'Username or password is incorrect'};
+                .error(function (res) {
+                    response = {success: false, data: res};
                     callback(response);
                 });
         }
