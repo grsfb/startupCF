cff464519d50e48807bb5e1ffcb24411f960c144
‘gaurav’ <gauravorgrs@gmail.com>
2016-03-10 22:29:24 +0530

    issue with cart after login

--- pages/login/loginController.js
+++ pages/login/loginController.js
@@ -3,8 +3,8 @@
     angular
         .module('Chefonia')
         .controller('LoginController', LoginController);
-    LoginController.$inject = ['SessionService','UserService', 'AuthenticationService', 'CartService', 'CommonService', '$location','EventHandlingService'];
-    function LoginController(SessionService,UserService, AuthenticationService, CartService, CommonService,$location,EventHandlingService) {
+    LoginController.$inject = ['SessionService', 'UserService', 'AuthenticationService', 'CartManager', 'CommonService', '$location'];
+    function LoginController(SessionService, UserService, AuthenticationService, CartManager, CommonService, $location) {
         var vm = this;
         vm.login = login;
         vm.passwordReset = passwordReset;
@@ -14,7 +14,7 @@
         vm.showMblSignup = false;
         vm.reset = reset;
         vm.pageMsg = "";
-        vm.doFBLogin=doFBLogin;
+        vm.doFBLogin = doFBLogin;
         function login() {
             vm.isLogging = true;
 
@@ -23,15 +23,15 @@
                     $('#myModal').modal('hide');
                     //for mobile view
                     AuthenticationService.SetCredentials(response.data);
-                    EventHandlingService.eventForUpdateBagSelectionBroadcast(true);
+                    CartManager.loadUserBag(undefined);
                     $location.path('home');
-                    //updateUserCart(response.data);
                 } else {
                     vm.pageMsg = "User email or password is incorrect";
                 }
                 vm.isLogging = false;
             });
         }
+
         function isUserLoggedIn() {
             return SessionService.get(SessionService.Session.CurrentUser);
         }
@@ -53,6 +53,7 @@
                 vm.isPasswordChanging = false;
             })
         }
+
         function statusChangeCallback(response) {
             // The response object is returned with a status field that lets the
             // app know the current login status of the person.
@@ -62,18 +63,18 @@
                 // Logged into your app and Facebook.
 
                 var accessToken = response.authResponse.accessToken;
-                getFBUserDataAPI(response.authResponse.accessToken,response.authResponse.userID);
+                getFBUserDataAPI(response.authResponse.accessToken, response.authResponse.userID);
 
             } else {
                 // The person is logged into Facebook, but not your app.
-                FB.login(function(response){
+                FB.login(function (response) {
                     if (response.status === 'connected') {
                         // Logged into your app and Facebook.
-                        getFBUserDataAPI(response.authResponse.accessToken,response.authResponse.userID);
+                        getFBUserDataAPI(response.authResponse.accessToken, response.authResponse.userID);
                     }
                     // Handle the response object, like in statusChangeCallback() in our demo
                     // code.
-                },{scope:'email'});
+                }, {scope: 'email'});
             }
         }
 
@@ -82,58 +83,61 @@
         // code below.
         function checkLoginState() {
 
-            FB.getLoginStatus(function(response) {
+            FB.getLoginStatus(function (response) {
                 if (response.status === 'connected') {
                     // Logged into your app and Facebook.
 
                     var accessToken = response.authResponse.accessToken;
-                    getFBUserDataAPI(response.authResponse.accessToken,response.authResponse.userID);
+                    getFBUserDataAPI(response.authResponse.accessToken, response.authResponse.userID);
 
                 }
             });
         }
+
         function doFBLogin() {
 
-            FB.getLoginStatus(function(response) {
+            FB.getLoginStatus(function (response) {
                 statusChangeCallback(response);
             });
         }
-        window.fbAsyncInit = function() {
+
+        window.fbAsyncInit = function () {
             FB.init({
-                appId      : '1721100084768505',
-                cookie     : true,  // enable cookies to allow the server to access
-                channelUrl : 'localhost/client/channel.html',      // the session
-                xfbml      : true,  // parse social plugins on this page
-                version    : 'v2.5' // use version 2.2
+                appId: '1721100084768505',
+                cookie: true,  // enable cookies to allow the server to access
+                channelUrl: 'localhost/client/channel.html',      // the session
+                xfbml: true,  // parse social plugins on this page
+                version: 'v2.5' // use version 2.2
             });
-            if(isUserLoggedIn()==false) {
+            if (isUserLoggedIn() == false) {
                 checkLoginState();
             }
         };
 
         // Load the SDK asynchronously
-        (function(d, s, id) {
+        (function (d, s, id) {
             var js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
+            js = d.createElement(s);
+            js.id = id;
             js.src = "//connect.facebook.net/en_US/sdk.js";
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
 
         // Here we run a very simple test of the Graph API after login is
         // successful.  See statusChangeCallback() for when this call is made.
-        function getFBUserDataAPI(accessToken,userId) {
-            FB.api("/me?fields=id,name,email",function (response) {
-                    var fbName=response.name;
-                    var fbEmail= response.email;
-                    var user = new User(userId,fbName,userId, fbEmail);
+        function getFBUserDataAPI(accessToken, userId) {
+            FB.api("/me?fields=id,name,email", function (response) {
+                    var fbName = response.name;
+                    var fbEmail = response.email;
+                    var user = new User(userId, fbName, userId, fbEmail);
                     UserService.create(user, function (response) {
                         if (response.success) {
                             AuthenticationService.Login(fbEmail, userId, function (response) {
                                 if (response.success) {
                                     AuthenticationService.SetCredentials(response.data);
                                     $('#myModal').modal('hide');
-                                    EventHandlingService.eventForUpdateBagSelectionBroadcast(true);
+                                    CartManager.loadUserBag(undefined);
                                     $location.path('home');
                                     //updateUserCart(response.data);
                                 }
@@ -145,9 +149,8 @@
 
                                     //for mobile view
                                     AuthenticationService.SetCredentials(response.data);
-                                    EventHandlingService.eventForUpdateBagSelectionBroadcast(true);
+                                    CartManager.loadUserBag(undefined);
                                     $location.path('cart');
-                                    //updateUserCart(response.data);
                                 } else {
                                     vm.pageMsg = "User email or password is incorrect";
                                 }
@@ -156,18 +159,19 @@
                         }
                         vm.isRegistering = false;
                     });
-
                 }
             );
 
         }
-        function User(userId,name, password, email) {
-            this.userId=userId;
+
+        function User(userId, name, password, email) {
+            this.userId = userId;
             this.name = name;
             this.password = password;
             this.email = email;
         }
     }
+
     // This is called with the results from from FB.getLoginStatus().
 
 })();
